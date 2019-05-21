a02bd9f41bedc2d850769897c398973d634f9bf3
Michelle Moon <michellemoon236@gmail.com>
2019-05-21 22:19:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,2 +1,13 @@
 # Create your haunted_houses migration here
 
+class CreateHauntedHouse < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+end
