c6313d3eadd4bb5b08a30596ff7a1cd704d3ec82
Michelle Moon <michellemoon236@gmail.com>
2019-05-21 22:11:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,14 @@
 # Create your costume_stores migration here
 
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      
+      t.timestamps
+    end
+  end
+end
