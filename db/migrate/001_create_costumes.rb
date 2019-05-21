b97731d04959edadd2a0e21e38047540463ce18f
Michelle Moon <michellemoon236@gmail.com>
2019-05-21 22:07:27 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,9 +10,9 @@
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
-    create_table :candies do |t|
+    create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.text :size 
       t.text :image_url
       t.timestamps
